983ebd4f9d82e3decd48965cc17f7a52cf26e850
kartikeyachaauhan <kc1.408596@gmail.com>
2019-02-17 15:28:00 +0530

    Initial Commit

---
+++ GetItDone/WelcomeController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  GetItDone
+//
+//  Created by Kartikeya Chauhan on 16/02/19.
+//  Copyright © 2019 Jungian Typology. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeController: UIViewController {
+
+    let bg:UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .cyan
+        view.layer.cornerRadius = 6
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(bg)
+        bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive=true
+        bg.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive=true
+        bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive=true
+        bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive=true
+        
+    }
+    
+
+}
+
