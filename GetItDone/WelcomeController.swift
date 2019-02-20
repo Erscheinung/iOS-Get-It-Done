476d8b75624d47ef04d25b495f408d4cea6412ba
kartikeyachaauhan <kc1.408596@gmail.com>
2019-02-20 06:31:44 +0530

    Added Title Label manually

--- GetItDone/WelcomeController.swift
+++ GetItDone/WelcomeController.swift
@@ -16,14 +16,27 @@ class WelcomeController: UIViewController {
         return view
     }()
     
+    let titleLabel = GDLabel(title: "GET IT DONE", size: 24, textAlign: .center)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
+//        titleLabel.text = "GET IT DONE"
+//        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addSubview(bg)
         bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive=true
         bg.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive=true
         bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive=true
         bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive=true
+        
+        view.addSubview(titleLabel)
+        titleLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: bg.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: bg.topAnchor, constant: 60).isActive = true
+        
     }
 
 }
---
+++ GetItDone/custom/GDLabel.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  GetItDone
+//
+//  Created by Kartikeya Chauhan on 20/02/19.
+//  Copyright © 2019 Jungian Typology. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GDLabel:UILabel {
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(title:String = "Default text", color:UIColor = .white, size:CGFloat = 16, frame: CGRect = .zero, textAlign:NSTextAlignment = .left) {
+        super.init(frame: frame)
+        if frame == .zero {
+            self.translatesAutoresizingMaskIntoConstraints = false
+        }
+        self.text = title
+        self.textColor = color
+        self.font = UIFont.systemFont(ofSize: size)
+        self.textAlignment = textAlign
+    }
+}
