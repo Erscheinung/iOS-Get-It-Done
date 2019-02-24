1fff10d0de08c03843f78e879b03d31d38a41c29
kartikeyachaauhan <kc1.408596@gmail.com>
2019-02-24 15:15:55 +0530

    App-screen description label added.

--- GetItDone/WelcomeController.swift
+++ GetItDone/WelcomeController.swift
@@ -18,6 +18,12 @@ class WelcomeController: UIViewController {
     
     let titleLabel = GDLabel(title: "GET IT DONE", size: 24, textAlign: .center)
     
+    let infoLabel:UILabel = {
+        let label = GDLabel(title: "WELCOME. GET IT DONE IS A TO DO LIST.\nA REALLY DOPE TO-DO-LIST.", size: 14, textAlign: .center)
+        label.numberOfLines = 2
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,6 +42,12 @@ class WelcomeController: UIViewController {
         titleLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: bg.centerXAnchor).isActive = true
         titleLabel.topAnchor.constraint(equalTo: bg.topAnchor, constant: 60).isActive = true
+        
+        bg.addSubview(infoLabel)
+        infoLabel.centerXAnchor.constraint(equalTo: bg.centerXAnchor).isActive = true
+        infoLabel.centerYAnchor.constraint(equalTo: bg.centerYAnchor).isActive = true
+        infoLabel.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        infoLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
         
     }
 
