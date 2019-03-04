60f404680426f3c9d7d5d39fb91361baa4d48cc0
kartikeyachaauhan <kc1.408596@gmail.com>
2019-03-04 16:58:12 +0530

    Added custom protocol

--- GetItDone/controllers/ListController.swift
+++ GetItDone/controllers/ListController.swift
@@ -10,7 +10,11 @@ import Foundation
 
 import UIKit
 
-class ListController: UIViewController {
+class ListController: UIViewController, GDHeaderDelegate {
+    func addItem() {
+        print("trying to add item from header")
+    }
+    
     
     let header = GDHeaderView(title: "Stuff to get done", subtitle: "4 left")
     
@@ -23,5 +27,7 @@ class ListController: UIViewController {
         header.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         header.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         header.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        header.delegate = self
     }
 }
---
+++ GetItDone/extensions/protocol.swift
@@ -0,0 +1,13 @@
+//
+//  protocol.swift
+//  GetItDone
+//
+//  Created by Kartikeya Chauhan on 04/03/19.
+//  Copyright © 2019 Jungian Typology. All rights reserved.
+//
+
+import Foundation
+
+protocol GDHeaderDelegate {
+    func addItem()
+}
